e79b8a4f3d7d85edb3c0e2814e5ed9792fc76bc0
nagendre1 <139125499+nagendre1@users.noreply.github.com>
2025-03-06 18:57:01 +0530

    Update streamlit_app.py
    imported requests  lib and added get call

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -24,6 +26,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 5 ingredients : ' 
                                   , my_dataframe
                                   , max_selections = 5)
+
+smoothiefroot_response = requests.get('https://my.smoothiefroot.com/api/fruit/watermelon')
+st.text(smoothiefroot_response)
 
 if ingredients_list:
     
