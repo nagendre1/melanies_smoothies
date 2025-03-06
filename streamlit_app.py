39a0b0c77c9b81f5d1f7034b004f45f2dd873b03
nagendre1 <139125499+nagendre1@users.noreply.github.com>
2025-03-06 20:14:13 +0530

    Update streamlit_app.py
    integrated fruit_choosen from the list into API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,17 +32,15 @@ if ingredients_list:
     ingredients_list = ''
     
     #st.write(ingredients_list)
-
     #st.text(ingredients_list)
-
-    ingredients_string = ''
 
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
+        st.subheader(fruit_choosen + ' Nutrition Information')
 
     #st.text(ingredients_string)
 
-    smoothiefroot_response = requests.get('https://my.smoothiefroot.com/api/fruit/watermelon')
+    smoothiefroot_response = requests.get('https://my.smoothiefroot.com/api/fruit/' + fruit_choosen)
     #st.text(smoothiefroot_response.json())
     st_df = st.dataframe(data = smoothiefroot_response.json() , use_container_width = True)
 
